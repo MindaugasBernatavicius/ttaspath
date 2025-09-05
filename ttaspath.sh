cc0662471797dcef9cf89562fd2ba0bb34c3fe48
M1ndas <darbas.mindaugas@gmail.com>
2025-09-05 16:25:38 +0300

    Update ttaspath.sh

--- ttaspath.sh
+++ ttaspath.sh
@@ -1,4 +1,1 @@
-bash -c 'd="$1"; traceroute -Tnp 443 "$d" |\
-  awk "/^[ 0-9]+ / {for(i=2;i<=NF;i++) if (\$i ~ /^[0-9.]+$/) {print \$i; break}}" |\
-  while read -r ip; do whois -h whois.cymru.com "$ip" |\
-  awk "NR>1{print}"; done' _ "$DOMAIN"
+bash -c 'd="$1"; echo "$d"' _ "$DOMAIN"
